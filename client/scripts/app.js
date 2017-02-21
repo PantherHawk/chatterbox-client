a17b965d4a71bce79b08977f3cf12777466ac1fd
Alex Rosenthal <alexrosenthalster@gmail.com>
2017-02-20 17:22:17 -0800

    modified send and fetch functions

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -9,28 +9,28 @@ app.init = function() {
 
 app.send = function(message) {
   $.ajax({
-      url: app.server,
-      type: 'POST',
-      data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: function(data) {
-        console.log('chatterbox: message sent, ' + data);
-      },
-      error: function(data) {
-        console.log(data);
-      }
-    });
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function(data) {
+      console.log('chatterbox: message sent, ' + data);
+    },
+    error: function(data) {
+      console.log(data);
+    }
+  });
 };
 
 app.fetch = function() {
   $.ajax({
-      url: app.server,
-      type: 'GET',
-      success: function(data) {
-        console.log(data);
-      },
-      error: function(data) {
-        console.log(data);
-      }
-    });
+    url: app.server,
+    type: 'GET',
+    success: function(data) {
+      console.log(data);
+    },
+    error: function(data) {
+      console.log(data);
+    }
+  });
 };
